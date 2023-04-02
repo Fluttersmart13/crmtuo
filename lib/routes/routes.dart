43f1d6c489e40c0ec9e0ca6384daf022e01e50b6
Soni bind <sonigithubproject@gmail.com>
2023-04-02 08:31:09 +0530

    job details new changes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,7 +1,7 @@
 import 'package:crmtuo/bloc/logics/login_bloc.dart';
 import 'package:crmtuo/routes/router_constants.dart';
+import 'package:crmtuo/view/home_screen.dart';
 import 'package:crmtuo/view/login_screen.dart';
-import 'package:crmtuo/view/webView.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
